16cbddac863125ae431d9a1e9119fc6ee2230453
Christina Huang <cchristina.huang@mail.utoronto.ca>
2024-11-10 18:07:45 -0400

    Added Constants

---
+++ src/main/java/data_access/Constants.java
@@ -0,0 +1,9 @@
+package data_access;
+
+/**
+ * Constants used in data access.
+ */
+public class Constants {
+    public static final String LANGUAGE_KEY = "language";
+    public static final String TEXT_KEY = "text";
+}
