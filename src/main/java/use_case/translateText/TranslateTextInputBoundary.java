5d90ae41cc3cf48447017d04ff95a364cd4c683b
903Edan <edan.wong@mail.utoronto.ca>
2024-11-11 14:31:43 -0500

    Implement TranslateTextInputData and modify the execute method in TranslateTextInteractor

--- src/main/java/use_case/translateText/TranslateTextInputBoundary.java
+++ src/main/java/use_case/translateText/TranslateTextInputBoundary.java
@@ -7,10 +7,8 @@ public interface TranslateTextInputBoundary {
 
     /**
      * Executes the translatetext use case.
-     * @param inputText the text to be translated.
-     * @param inputLanguage  the language name of text (ie. English). Set to null to detect language.
-     * @param outputLanguage the language name (ie. English) to which the text is translated
+     * @param translateTextInputData the input data.
      */
-    void execute(String inputLanguage, String inputText, String outputLanguage);
+    void execute(TranslateTextInputData translateTextInputData);
 
 }
