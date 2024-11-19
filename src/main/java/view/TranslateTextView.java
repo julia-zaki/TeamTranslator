52462f0bdef836076ebd4e38b926d56ec9d8f6bd
Christina Huang <cchristina.huang@mail.utoronto.ca>
2024-11-19 18:48:29 -0400

    fixed some CheckStyle errors

--- src/main/java/view/TranslateTextView.java
+++ src/main/java/view/TranslateTextView.java
@@ -1,13 +1,24 @@
 package view;
 
-import java.awt.*;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.List;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import interface_adapter.imageUpload.ImageUploadController;
